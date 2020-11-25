4f97a735c9bc21172279e6b2c33bf06b7a7d1e68
francesco <f.iacono89@gmail.com>
2020-11-25 15:57:35 +0100

    primo js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function () {
 
 
-
-
-});
+    $('.header-bottom-end .list-inline li a').mouseenter(function () {
+      console.log($(this));
+      console.log($(this).next());
+      $(this).next().toggleClass('active');
+      }
+    );
+    $('.header-bottom-end .list-inline li a').mouseleave(function () {
+      console.log($(this));
+      console.log($(this).next());
+      $(this).next().toggleClass('active');
+      }
+    );
+  }
+);
