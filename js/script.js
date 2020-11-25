7414656a48190ac9ca6e7c755fc10158eeac2e4f
francesco <f.iacono89@gmail.com>
2020-11-25 19:58:20 +0100

    non ho finito

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 $(document).ready(function () {
 
-
-    $('.header-bottom-end .list-inline li a').mouseenter(function () {
-      console.log($(this));
-      console.log($(this).next());
-      $(this).next().toggleClass('active');
+    // hover li nel menu header-bottom-end
+    $('.header-bottom-end .list-inline > li:nth-child(1n+2)').mouseenter(function () {
+      console.log($(this).children());
+      $(this).children().toggleClass('active');
       }
     );
-    $('.header-bottom-end .list-inline li a').mouseleave(function () {
-      console.log($(this));
-      console.log($(this).next());
-      $(this).next().toggleClass('active');
+    $('.header-bottom-end .list-inline > li:nth-child(1n+2)').mouseleave(function () {
+
+      $(this).children().toggleClass('active');
+
+      }
+    );
+    // fine hover li nel menu header-bottom-end
+    $('#position-static').mouseenter(function () {
+      console.log($('#position-static').children());
+      $('#position-static').children().toggleClass('active-flex');
       }
     );
   }
